9c941757d182308dab59d203f59a11aa9ff59ae8
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-12-18 12:14:53 +0100

    Initial Commit

---
+++ OpenAi/OpenAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAiApp.swift
+//  OpenAi
+//
+//  Created by Michele Manniello on 18/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
